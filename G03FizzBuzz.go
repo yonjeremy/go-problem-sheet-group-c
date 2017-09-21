84be178f3fe0588b7422bb10ef517c8f4d3c44b3
jeremy yon <jeremy yon>
2017-09-21 17:00:07 +0100

    second commit

---
+++ G03FizzBuzz.go
@@ -0,0 +1,21 @@
+//FizzBuzz code in GO language
+
+package main
+
+import (
+    "fmt"
+)
+
+func main(){
+	for i := 1; i <= 100; i++ {
+        if ( (i % 5 == 0) && (i%3 == 0)){
+        fmt.Println("FizzBuzz")
+        } else if ( i % 3 == 0){
+            fmt.Println("Fizz")
+        } else if ( i % 5 == 0){
+            fmt.Println("Buzz")
+        }  
+	    fmt.Println(i)
+
+    }
+}
