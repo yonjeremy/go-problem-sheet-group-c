41172277ebb5434d20743363efa7a99b9b5857bc
jeremy yon <jeremy yon>
2017-09-21 16:10:36 +0100

    third commit

--- G02TimeAndDate.go
+++ G02TimeAndDate.go
@@ -1,3 +1,7 @@
+// Current time and date in Go
+// Author Jeremy
+// This is where I got it from  https://tour.golang.org/welcome/4
+
 package main
 
 import (
@@ -8,5 +12,5 @@ import (
 func main() {
 	fmt.Println("Welcome to the playground!")
 
-	fmt.Println("The time is", time.Now())
+	fmt.Println("The time and date is", time.Now())
 }
