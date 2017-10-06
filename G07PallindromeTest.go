f77bd240a5886d68f2a1173cdd15907b5d262641
jeremy yon <jeremy yon>
2017-10-06 14:38:21 +0100

    2nd Commit

--- G07PallindromeTest.go
+++ G07PallindromeTest.go
@@ -1,4 +1,4 @@
-//Guessing game
+//Test if user input String is a Pallindrome
 //Author Jeremy Yon G00330435
 
 package main
@@ -9,10 +9,12 @@ import (
 
 func main(){
     
+    // Prompt for user input String
     var input string
     fmt.Println("Enter String to test for Palindrome:")
     fmt.Scanf("%s\n", &input)
 
+    // Calls the test Pallindrome function
     fmt.Printf("Test input for Palindrome: %t",testPalindrome(input))
 
 }
@@ -20,12 +22,14 @@ func main(){
 func testPalindrome(input string) bool{
     last := len(input) - 1
    
+    // compares first char in array with last char, 2nd with 2nd to last char and so forth
     for i :=0; i < (len(input))/2;i++{
         if input[i] != input[last - i]{
             return false
         }
     }
 
+    // prints true if all characters matched in order
     return true
 
 }
