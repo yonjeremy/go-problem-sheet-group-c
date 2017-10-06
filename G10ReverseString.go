75349e6c0a43b3ab9f9be3f42f2bb48d551f2852
jeremy yon <jeremy yon>
2017-10-06 14:56:17 +0100

    2nd Commit

--- G10ReverseString.go
+++ G10ReverseString.go
@@ -1,5 +1,6 @@
-//Guessing game
+//Reverse String
 //Author Jeremy Yon G00330435
+//Code extracted from https://stackoverflow.com/questions/1752414/how-to-reverse-a-string-in-go
 
 package main
 
@@ -8,33 +9,23 @@ import (
 )
 
 func main(){
-    
+    // Prompt for user input String
     var input string
     fmt.Println("Enter String to reverse it:")
     fmt.Scanf("%s\n", &input)
 
+    // call function to reverse string
     fmt.Printf("The reversed string is: %s",reverse(input))
 
 }
 
-// func reverseString(input string) string{
-//     last := len(input) - 1
-// 	var temp byte
-   
-//     for i :=0; i < (len(input))/2;i++{
-//         temp = input[i]
-// 		input[i] = input[last-i]
-// 		input[last-i] = temp
-//     }
-
-//     return input
-
-// }
-
 func reverse(s string) string {
+    // use rune to store characters and switch places with each other
 	chars := []rune(s)
 	for i, j := 0, len(chars)-1; i < j; i, j = i+1, j-1 {
 		chars[i], chars[j] = chars[j], chars[i]
 	}
+
+    // return reversed string
 	return string(chars)
 }
