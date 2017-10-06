5494037b30525991a9f93e400c668f1be04fa615
jeremy yon <jeremy yon>
2017-10-06 14:12:50 +0100

    2nd commit

--- G03FizzBuzz.go
+++ G03FizzBuzz.go
@@ -8,14 +8,19 @@ import (
 )
 
 func main(){
+    // Loops 100 times
 	for i := 1; i <= 100; i++ {
+
+        // print FIZZBUZZ if multiples of 3 and 5
         if ( (i % 5 == 0) && (i%3 == 0)){
         fmt.Println("FizzBuzz")
-        } else if ( i % 3 == 0){
+        } else if ( i % 3 == 0){ // print Fizz if multiples of 3
             fmt.Println("Fizz")
-        } else if ( i % 5 == 0){
+        } else if ( i % 5 == 0){ // print Buzz if multiples of 5
             fmt.Println("Buzz")
-        }  
+        }
+
+        // print results to console  
 	    fmt.Println(i)
 
     }
